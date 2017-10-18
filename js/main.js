02ee0e2bbc839eb7bca27aaf5ba93d7222c290fe
dvinubius <dvinubiu.narmon@gmail.com>
2017-10-18 22:48:05 +0300

    added smooth scrolling and collapse navbar behaviour improvement - when clicked/tapped anywhere, the expanded navbar collapses again

--- js/main.js
+++ js/main.js
@@ -6,40 +6,54 @@ var delta = Math.round(navbarHeight / 2);
 
 
 window.onload = function() {
-	
+	// init carousel
 	$(".carousel").carousel({
 		interval: 2000,
 		pause:false,
 		wrap: true,
 		keyboard: true
 	});
-	
-	
-	setInterval(function() {  
-		
+
+	// listen for clicks in order to hide expanded collapsed navbar
+	$("body").on('click', collapseNavbar);
+
+	// listen for scroll events in order to hide/show
+	// navbar and for correct footer display
+	setInterval(function() {
+
 		/*  for the correct display of the footer (with no banner underneath)*/
-		
+
 		var scrolled = document.documentElement.scrollTop;
 		var total = document.documentElement.offsetHeight;
 		var vh = window.innerHeight;
 		var footer = document.getElementsByTagName('footer')[0].offsetHeight;
-		
-		if (total - (scrolled + vh) < footer) {  // footer is visible			
+
+		if (total - (scrolled + vh) < footer) {  // footer is visible
 			hideBanner();
 		}
 		if (scrolled < vh) {  // banner is visible
 			showBanner();
 		}
-		
-		
+
+
 		/*  for the scroll-toggle of the navbar*/
 		if (didScroll) {
         hasScrolled();
         didScroll = false;
     }
-		
+
 	}, 150);
+
+	// init events for smooth scrolling
+	$(".navButton").click(function(ev) {
+		ev.preventDefault();
+		var targetID = this.getAttribute('href');
+		document.getElementById(targetID.substr(1)).scrollIntoView({
+		  behavior: 'smooth'
+		});
+	});
 }
+
 
 
 /* scroll detection*/
@@ -50,14 +64,14 @@ $(window).scroll(function(event){
 
 /* REACT to scrolling by toggling navbar visibility */
 function hasScrolled() {
-	
+
     var st = $(this).scrollTop();
-			
-    
+
+
     // Make sure they scroll more than delta
     if(Math.abs(lastScrollTop - st) <= delta)
         return;
-    		
+
     // If they scrolled down and are past the navbar, add class .nav-up.
     // This is necessary so you never see what is "behind" the navbar.
     if (st > lastScrollTop && st > navbarHeight){
@@ -69,14 +83,14 @@ function hasScrolled() {
             $('.navbar').removeClass('nav-up');
         }
     }
-    
+
     lastScrollTop = st;
 }
 
 
 
 /* make buttons lose focus after being pressed */
-function buttonHandler(ev) {	
+function buttonHandler(ev) {
 	console.log(ev.target);
 	ev.target.blur();
 }
@@ -87,4 +101,14 @@ function hideBanner() {
 
 function showBanner() {
 	$(".banner").show();
+}
+
+function collapseNavbar(ev) {
+	if (ev.target != $('.navbar-toggle')[0]) {
+		var col = $(".collapse");
+		if (col.hasClass('in'))	{
+			col.removeClass('in');
+			col.attr('aria-expanded', 'false');
+		}
+	}
 }
